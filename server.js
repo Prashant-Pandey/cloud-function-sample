d019ccf71dbdfe7a12b44c16cce86ac62096dea6
Prashant Pandey <prashant.pandey.dev@gmail.com>
2020-10-14 14:21:54 -0400

    added axios support

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const app = require("express")();
-const http = require("http")
+const axios = require("axios");
+const https = require("https");
 const URL = 'https://us-central1-ssdi-team-mobility.cloudfunctions.net/ITIS-6177-Week06?keyword='
-app.get('/', async (req, res) => {
+app.get('', async (req, res) => {
     try {
-        const msg = req.query.keyword || req.body.keyword || null;
+        const msg = req.query.keyword || null;
         if (msg === null) {
-            res.send('No keyword sent')
+            res.send('No keyword sent');
+            return;
         }
-        const response = await http.get(URL + msg);
+        const response = (await axios.get(URL + msg)).data;
+        res.status(200)
         res.send(response); 
     } catch (error) {
+        console.log('error', error);
         res.send(error);
     }
 
